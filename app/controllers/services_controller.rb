f52f096b8e26d4386021f267921f1c66e7674445
Tanguy Le Baud <tanguy.lebaud@gmail.com>
2017-11-28 14:55:55 +0100

    adding photo uploader feature

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,13 @@
 class ServicesController < ApplicationController
+  before_action :set_service
+
+  def show
+    @booking = Booking.new
+  end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
 end
