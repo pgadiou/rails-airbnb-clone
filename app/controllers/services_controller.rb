01071bc5e40c3f6cae88667e9ef0712d40175ea6
thomas quelin <th.quelin@gmail.com>
2017-11-28 16:04:00 +0100

    formulaire de création d'un service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,24 @@
 class ServicesController < ApplicationController
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def service_params
+    params.require(:service).permit(:name, :category, :location, :distance, :price)
+  end
+
 end
+
+
