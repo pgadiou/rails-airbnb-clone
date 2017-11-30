23427ce11e72a8e864d15a897818ad5bd0ec2b99
thomas quelin <th.quelin@gmail.com>
2017-11-30 16:33:53 +0100

    Searchbar côté backend OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @services = Service.where.not(latitude: nil)
-    if params[:query_what].present? && params[:query_where].present?
-      @services = Service.near(params[:query_where], 50, :order => false).where(category: params[:query_what])
+    max_distance = 10
+    q_where = params[:query_where]
+    q_what = params[:query_what]
+    if q_where.present? && q_what.present?
+      @services = Service.where.not(latitude: nil).near(q_where, max_distance, order: false).where(category: q_what)
+    elsif q_what.present?
+      @services = Service.where(category: q_what)
+    elsif q_where.present?
+      @services = Service.where.not(latitude: nil).near(q_where, max_distance, order: false)
     else
       @services = Service.all
     end
