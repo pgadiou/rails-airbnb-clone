438007bf61b4ebf5046f74632ef8ce7d5647aef4
thomas quelin <th.quelin@gmail.com>
2017-11-30 15:49:14 +0100

    bug pending geocoder

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @services = Service.where.not(latitude: nil)
+    if params[:query_what].present? && params[:query_where].present?
+      @services = Service.near(params[:query_where], 50, :order => false).where(category: params[:query_what])
+    else
+      @services = Service.all
+    end
   end
 end
